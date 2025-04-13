eee22e002ce8690ba4797f38c1449681fecf200a
sivasuribabupenkey <sivasuribabupenkey@gmail.com>
2025-04-13 12:38:24 +0530

    update env files

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -4,19 +4,20 @@ provider "aws" {
 
 module "s3" {
   source       = "../../modules/s3"
-  env_name     = "dev"
+  env_name     = "prod"
 }
 
 module "vpc" {
   source   = "../../modules/vpc"
-  env_name = "dev"
+  env_name = "prod"
   region   = var.region
   azs      = ["us-east-1a", "us-east-1b"]
 }
 
 module "asg" {
   source             = "../../modules/asg"
-  env_name           = "dev"
+  env_name           = "prod"
   private_subnet_ids = module.vpc.private_subnet_ids
 }
+
 
